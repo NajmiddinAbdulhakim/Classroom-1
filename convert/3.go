48254b2ffa3e9aa823f7797e8c3e43d27bd2eb5c
Najmiddin <najmiddin@example.com>
2022-05-14 23:28:57 +0500

    conv is over

--- convert/3.go
+++ convert/3.go
@@ -3,25 +3,20 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	a, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	a = strings.Replace(a, ",", ".", -1)
-	a = strings.Replace(a, " ", "", -1)
-	// var g float64
-	var b []string
-	b = strings.Split(a, ";")
-	fmt.Println(b[1])
+	a, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil && err != io.EOF {
+		fmt.Printf("Error reading | ", err)
+	}
+	b := strings.Split(strings.ReplaceAll(strings.ReplaceAll(a, " ", ""), ",", "."), ";")
 	g, _ := strconv.ParseFloat(b[0], 64)
 	h, _ := strconv.ParseFloat(b[1], 64)
-	fmt.Println(g, h)
-	fmt.Println(g / h)
-	// for _, c := range b {
-	// 	g = c
-	// }
-	// fmt.Printf("%f", 1149.6088607594936/1179.0666666666666)
+	fmt.Printf(strconv.FormatFloat(g/h, 'f', 4, 64))
+
 }
