9870c9d8f2d315cc1a42248a221744c18a7955ca
Najmiddin <najmiddin@example.com>
2022-05-14 20:42:44 +0500

    map is over

--- map/1.go
+++ map/1.go
@@ -1,6 +1,19 @@
 package main
-import ("fmt")
+
+import (
+	"fmt"
+)
+
 func main() {
 	m := make(map[int]int)
-	for i,val := range m {}
+	var a int
+	for i := 0; i < 10; i++ {
+		fmt.Scan(&a)
+		if _, ok := m[a]; ok {
+			fmt.Print(m[a], " ")
+		} else {
+			m[a] = work(a) // ready func
+			fmt.Print(m[a], " ")
+		}
+	}
 }
--- map/2.go
+++ map/2.go
@@ -1,4 +1,37 @@
+package main
 
-for key,val := range groupCity[100] {
-	cityPopulation[key] = val
+import (
+	"fmt"
+)
+
+func main() {
+	// groupCity := map[int][]string{
+	// 	10:   []string{"hello", "world", "south"}, // города с населением 10-99 тыс. человек
+	// 	100:  []string{"hello", "world"},          // города с населением 100-999 тыс. человек
+	// 	1000: []string{"hello", "world", "south"}, // города с населением 1000 тыс. человек и более
+	// }
+
+	// cityPopulation := map[string]int{
+	// 	"hello": 1,
+	// 	"world": 2,
+	// 	"south": 3,
+	// }
+
+	for k, _ := range cityPopulation {
+		cou := 0
+		for _, v := range groupCity[100] {
+			if k == v {
+				cou++
+			}
+		}
+		if cou != 1 {
+
+			delete(cityPopulation, k)
+		}
+	}
+	fmt.Print(cityPopulation)
 }
+
+// for _, city := range append(groupCity[10], groupCity[1000]...) {
+//     delete(cityPopulation, city)
+// }
